735ea2a1a77f85571fe11b87ebe86e1d1fd3785f
jmcs1983 <jmvcf83@gmail.com>
2021-04-07 11:01:40 +0200

    1 file modified

--- index.js
+++ index.js
@@ -13,7 +13,19 @@ var datos = [
     {"periodo": "Febrero(01-07)", "name":"Casos COVID Residencias", "Nºcasos": 61},
     {"periodo": "Marzo(08-14)", "name":"Casos COVID Residencias", "Nºcasos": 87},
     {"periodo": "Marzo(15-21)", "name":"Casos COVID Residencias", "Nºcasos": 49},
-    {"periodo": "Marzo(22-28)", "name":"Casos COVID Residencias", "Nºcasos": 34}
+    {"periodo": "Marzo(22-28)", "name":"Casos COVID Residencias", "Nºcasos": 34},
+    {"periodo": "Enero(04-10)", "name":"Fallecidos COVID Residencias", "Nºcasos": 365},
+    {"periodo": "Enero(11-17)", "name":"Fallecidos COVID Residencias", "Nºcasos": 536},
+    {"periodo": "Enero(18-24)", "name":"Fallecidos COVID Residencias", "Nºcasos": 730},
+    {"periodo": "Enero(25-31)", "name":"Fallecidos COVID Residencias", "Nºcasos": 790},
+    {"periodo": "Febrero(01-07)", "name":"Fallecidos COVID Residencias", "Nºcasos": 628},
+    {"periodo": "Febrero(08-14)", "name":"Fallecidos COVID Residencias", "Nºcasos": 390},
+    {"periodo": "Febrero(15-21)", "name":"Fallecidos COVID Residencias", "Nºcasos": 178},
+    {"periodo": "Febrero(22-28)", "name":"Fallecidos COVID Residencias", "Nºcasos": 80},
+    {"periodo": "Febrero(01-07)", "name":"Fallecidos COVID Residencias", "Nºcasos": 40},
+    {"periodo": "Marzo(08-14)", "name":"Fallecidos COVID Residencias", "Nºcasos": 23},
+    {"periodo": "Marzo(15-21)", "name":"Fallecidos COVID Residencias", "Nºcasos": 17},
+    {"periodo": "Marzo(22-28)", "name":"Fallecidos COVID Residencias", "Nºcasos": 23}
   ]
 
 var visualization = d3plus.viz()
